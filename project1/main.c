3ec075ee5ecf32c4ec357e991df71a9bab2b7d14
leewc <gusdn05229@gmail.com>
2023-04-16 15:57:02 +0900

    comment added

--- project1/main.c
+++ project1/main.c
@@ -5,19 +5,32 @@
 
 int main()
 {
+	//redirection file var.
 	int fd_in, fd_out;
+
+	//input file open
 	fd_in = open("input", O_RDONLY);
-	if(fd_in<0)
+	
+	//error case
+	if(fd_in < 0)
 	{
 		fprintf(stderr, "file1 open failed\n");
 	}
+
+	//output file open
 	fd_out = open("result", O_WRONLY | O_CREAT | O_TRUNC, 0644);
-	if(fd_in<0)
+
+	//error case
+	if(fd_in < 0)
 	{
 		fprintf(stderr, "file2 open failed\n");
 	}
+
+	//file descriptor redirection
 	dup2(fd_in, 0);
 	dup2(fd_out, 1);
+
+	//fork, execution program at child process
 	pid_t pid=fork();
 	if(pid == 0)
 	{
